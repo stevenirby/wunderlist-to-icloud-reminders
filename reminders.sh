816668e970984391e1e720282c2173eb5fa95407
Steven Irby <steven.a.irby@gmail.com>
2019-08-20 11:08:45 +0200

    Added comment to code

--- reminders.sh
+++ reminders.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 $(which wunderline) export | $(which jq) --raw-output '.data.lists[].tasks[] | select(.completed == false) .title' > /tmp/reminders.txt
+# AppleScript in workflow is largely just this code slightly modified
+# https://gist.github.com/n8henrie/c3a5bf270b8200e33591
 $(which automator) create_reminders.workflow
 
 exit 0
